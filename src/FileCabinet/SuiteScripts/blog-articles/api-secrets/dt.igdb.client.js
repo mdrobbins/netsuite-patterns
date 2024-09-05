2ef1c2ab7b1b65f7f6f81544a13958e1daef9944
mrobbins <mdrobbins@mdrnetworks.com>
2024-09-05 08:49:48 -0500

    Update IGDB library to support new OAuth2 requirement

--- src/FileCabinet/SuiteScripts/blog-articles/api-secrets/dt.igdb.client.js
+++ src/FileCabinet/SuiteScripts/blog-articles/api-secrets/dt.igdb.client.js
@@ -35,7 +35,8 @@ define(['N/https', 'N/log', 'N/runtime'], function (https, log, runtime) {
         const script = runtime.getCurrentScript();
 
         const clientId = script.getParameter({ name: 'custscript_dt_igdb_client_id' });
-        const token = script.getParameter({ name: 'custscript_dt_igdb_token' });
+
+        const token = getToken();
 
         const headers = {
             'Client-ID': clientId,
@@ -47,6 +48,18 @@ define(['N/https', 'N/log', 'N/runtime'], function (https, log, runtime) {
         const response = https.post({ url, headers, body });
 
         return JSON.parse(response.body);
+    }
+
+    function getToken() {
+        const url = https.createSecureString({
+            input: 'https://id.twitch.tv/oauth2/token?client_id=\{custsecret_dt_igdb_client_id\}&client_secret=\{custsecret_dt_igdb_client_secret\}&grant_type=client_credentials'
+        });
+
+        const response = https.post({ url });
+
+        const body = JSON.parse(response.body);
+
+        return body.access_token;
     }
 
     return {
